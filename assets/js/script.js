255fd800815cc2793d5a5c68a93a7e7f60e8c7ed
fpatrick <fpatrick.erick@gmail.com>
2022-03-09 22:09:30 +0000

    do BMI calculator

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,19 @@
-let form = document.getElementById("tdeeForm");
-// form.addEventListener('submit', handleSubmit);
-
-
-//Build metric or imperial system for height and weight
+//Build metric or imperial system for height and weight on start
 
 let heightWeight = document.getElementById("heightWeight");
-buildMetric();
-
 let metric = document.getElementById("metricSystem");
-metric.addEventListener("click", buildMetric);
-
 let imperial = document.getElementById("imperialSystem");
+
+// // Check to build so it build correctly if browser is refreshed
+if (metric.checked) {
+    buildMetric();
+} else if (imperial.checked) {
+    buildImperial();
+}
+
+//Build metric or imperial system for height and weight after click
+
+metric.addEventListener("click", buildMetric);
 imperial.addEventListener("click", buildImperial);
 
 function buildMetric(event) {
@@ -77,3 +80,64 @@ function buildImperial(event) {
         </div>
     `
 }
+
+// Listen to Submit form event
+
+let form = document.getElementById("tdeeForm");
+form.addEventListener('submit', handleSubmit);
+
+// Convert Imperial to metric 
+function convertImperial(iHeightF, iHeightIn, iWeight) {
+    let feetCM = iHeightF * 30.48;
+    let inchesCM = iHeightIn * 2.54;
+    let heightConverted = Math.floor(feetCM + inchesCM);
+    let weightKG =  Math.floor(iWeight / 2.205);
+    let result = {
+        height: heightConverted,
+        weight: weightKG
+    }
+    return result;
+}
+
+// Calculate BMI
+function calcBMI(height, weight) {
+    // Convert cm to meters
+    height = height / 100;
+    // pow gives meters squared - round ((value) * 100) / 100 so I can get 2 decimal places 
+    return Math.round( (weight / Math.pow(height, 2)) * 100) / 100;
+}
+
+// on submit
+function handleSubmit(event){
+    event.preventDefault();
+
+    let person = {};
+    person.activity = document.getElementById("activity").value;
+
+    let male = document.getElementById("male");
+    let female = document.getElementById("female");
+    if (male.checked) {
+        person.gender = "male";
+    } else if (female.checked) {
+        person.gender = "female"
+    }
+
+    if (imperial.checked) {
+        let heightFeet = document.getElementById("heightFeet").value;
+        let heightInches = document.getElementById("heightInches").value;
+        let weightPounds = document.getElementById("weightPounds").value;
+
+        let convertion = convertImperial(heightFeet, heightInches, weightPounds);
+        person.height = convertion.height;
+        person.weight = convertion.weight;
+
+    }  else {
+        person.height = document.getElementById("height").value;
+        person.weight = document.getElementById("weight").value;
+    }
+
+    alert(person.height);
+    //form.submit();
+}
+
+alert(calcBMI(165, 68));
